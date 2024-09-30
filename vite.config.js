01794dd1b3a4fa99b18ba1ed27c8bd73a8df71d2
juliovillanuevaf <fariasjulio789@gmail.com>
2024-09-30 00:13:13 -0500

    30//09/2024

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Divide los módulos de node_modules en chunks separados
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0];
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 2000, // Ajusta el límite según sea necesario
+  }
+});
